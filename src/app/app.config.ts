7c1091d55fa3d5ec9a5bd5faf3b67a87fb454cb1
Alfred Obialo <alfredcsdinc@gmail.com>
2024-03-20 15:27:52 +0100

    Configure app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimations} from "@angular/platform-browser/animations";
@@ -8,7 +8,7 @@ import {MessageService} from "primeng/api";
 import {DialogService} from "primeng/dynamicdialog";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient(),
     MessageService, DialogService
