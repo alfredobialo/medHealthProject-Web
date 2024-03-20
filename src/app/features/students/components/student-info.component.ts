2fdab4ecf8c7fe96e1b1a6291b5eb3ed079b8576
Alfred Obialo <alfredcsdinc@gmail.com>
2024-03-20 15:28:09 +0100

    Add Lineawesome icon resource

---
+++ src/app/features/students/components/student-info.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mh-student-info',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      student-info works!
+    </p>
+  `,
+  styles: ``
+})
+export class StudentInfoComponent {
+
+}
